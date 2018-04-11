b517011f34cbce1015a5f4e62e52f6c2dbb1c09f
Dominik22102 <37939144+Dominik22102@users.noreply.github.com>
2018-04-11 18:36:42 +0200

    Add files via upload

--- an.py
+++ an.py
@@ -3,10 +3,5 @@ import time
 
 gpio.setmode(gpio.BCM)
 gpio.setup(22,gpio.OUT)
-
-try:
-	while(True):
-		gpio.output(22,gpio.HIGH)
-		time.sleep(1)
-		gpio.output(22,gpio.LOW)
-		time.sleep(1)
+gpio.cleanup()
+gpio.output(22,gpio.HIGH)
